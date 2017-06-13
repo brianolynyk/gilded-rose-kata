676123479ee848a22285015932992cf11df41e6b
Brian Olynyk <bolynyk@360insights.com>
2017-06-12 21:19:37 -0400

    Updates for discussion

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,31 @@
 describe("Gilded Rose", function() {
 
+  /**
+   * STEP 1: Fix any broken tests and add any that will help
+   * confirm existing functionality works as expected.
+   */
   it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
+  });
+
+  it("should degrade quality twice as fast after sell by date", function () {
+    items = [ new Item("foo", -1, 10)];
+    update_quality();
+    expect(items[0].quality).toEqual(8);
+  });
+
+  /**
+   * STEP 3: Write tests for new behaviour.
+   */
+  it("should degrade conjured items twice as fast", function() {
+    items = [ new Item("Conjured Wand of Conjuring", 10, 50),
+              new Item("Conjured Wand of Conjuring", 0, 50)];
+    update_quality();
+    expect(items[0].quality).toEqual(48);
+    expect(items[1].quality).toEqual(46);
+
   });
 
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,20 +4,63 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = []
+var items = [];
+
 
 function update_quality() {
+  /**
+   * STEP 2: Create constants for repeated strings that
+   * line up with business language for ease of keeping
+   * in your head.
+   */
+
+  /**
+   * General thoughts on how to improve this gnarly mess
+   * below.
+   * 
+   * Each business rule could be it's own function or set 
+   * of functions. One function for each property.  Each
+   * item would then run though the existing lists of function
+   * which would have their own criteria and would be applied
+   * accordingly.
+   * 
+   * Get rid of the mutation.  Rather than constantly mutating
+   * the items, let's use functional programming and immutable
+   * date to make things clearer.
+   * 
+   * Stylistically, semicolons should be everywhere.
+   * triple equals should be used everywhere.
+   * 
+   * Lastly, depending on the amount of rules a system like
+   * this would realistically have, perhaps investing in a 
+   * dedicated rules engine like Drools (https://www.drools.org/)
+   * would makes sense.
+   */
+  const BRIE = 'Aged Brie';
+  const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+  const LEGENDARY_ITEM = 'Sulfuras, Hand of Ragnaros';
+  const CONJURED_ITEM = 'conjured';
+
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (items[i].name != BRIE && items[i].name != BACKSTAGE_PASSES) {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+        if (items[i].name != LEGENDARY_ITEM) {
+          /**
+           * STEP 4a: Add new feature.  Ideally this should be DRY.
+           * But abstraction by clipboard was used as can be seen
+           * below.
+           */
+          if (items[i].name.toLowerCase().indexOf(CONJURED_ITEM) > -1) {
+            items[i].quality = items[i].quality - 2  
+          } else {
+            items[i].quality = items[i].quality - 1
+          }
         }
       }
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (items[i].name == BACKSTAGE_PASSES) {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
               items[i].quality = items[i].quality + 1
@@ -31,15 +74,22 @@ function update_quality() {
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+    if (items[i].name != LEGENDARY_ITEM) {
       items[i].sell_in = items[i].sell_in - 1;
     }
     if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (items[i].name != BRIE) {
+        if (items[i].name != BACKSTAGE_PASSES) {
           if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+            if (items[i].name != LEGENDARY_ITEM) {
+              /**
+               * Step 4b: TODO: DRY this up.
+               */
+              if (items[i].name.toLowerCase().indexOf(CONJURED_ITEM) > -1) {
+                items[i].quality = items[i].quality - 2;
+              } else {
+                items[i].quality = items[i].quality - 1;
+              }
             }
           }
         } else {
